bdaebf9c03aa4dc882e3bbb2f81a3f65b8734438
Tiwajope <tiwatomide01@gmail.com>
2026-03-11 02:08:42 +0100

    Remove interactive menu for GitHub Actions

--- cloud_storage_auto.sh
+++ cloud_storage_auto.sh
@@ -3,10 +3,7 @@
 RESOURCE_GROUP="myResourceGroup"
 LOCATION="eastus"
 STORAGE_ACCOUNT="tiwastorage$RANDOM"
-CONTAINER_NAME="files"
-LOG_FILE="storage.log"
-
-# Create Storage Account & Container 
+CONTAINER_NAME="files" 
 
 echo "Creating resource group..."
 az group create --name $RESOURCE_GROUP --location $LOCATION
@@ -31,72 +28,5 @@ az storage container create \
   --public-access blob
 
 echo "Storage setup complete!"
-echo "Storage account: $STORAGE_ACCOUNT"
-echo "Container: $CONTAINER_NAME"
-
-#Get Storage Key 
-
-STORAGE_KEY=$(az storage account keys list \
-  --resource-group $RESOURCE_GROUP \
-  --account-name $STORAGE_ACCOUNT \
-  --query '[0].value' -o tsv)
-
-# File Management Menu
-
-while true; do
-  echo ""
-  echo "Choose an action:"
-  echo "1) Upload file"
-  echo "2) Download file"
-  echo "3) List files"
-  echo "4) Delete file"
-  echo "5) Exit"
-  read -p "Enter option number: " option
-
-  case $option in
-    1)
-      read -p "Enter local file path to upload: " FILE
-      az storage blob upload \
-        --account-name $STORAGE_ACCOUNT \
-        --account-key $STORAGE_KEY \
-        --container-name $CONTAINER_NAME \
-        --name $(basename $FILE) \
-        --file $FILE
-      echo "$(date) - UPLOAD - $FILE" >> $LOG_FILE
-      ;;
-    2)
-      read -p "Enter file name to download: " FILE
-      az storage blob download \
-        --account-name $STORAGE_ACCOUNT \
-        --account-key $STORAGE_KEY \
-        --container-name $CONTAINER_NAME \
-        --name $FILE \
-        --file $FILE
-      echo "$(date) - DOWNLOAD - $FILE" >> $LOG_FILE
-      ;;
-    3)
-      az storage blob list \
-        --account-name $STORAGE_ACCOUNT \
-        --account-key $STORAGE_KEY \
-        --container-name $CONTAINER_NAME \
-        --output table
-      echo "$(date) - LIST FILES" >> $LOG_FILE
-      ;;
-    4)
-      read -p "Enter file name to delete: " FILE
-      az storage blob delete \
-        --account-name $STORAGE_ACCOUNT \
-        --account-key $STORAGE_KEY \
-        --container-name $CONTAINER_NAME \
-        --name $FILE
-      echo "$(date) - DELETE - $FILE" >> $LOG_FILE
-      ;;
-    5)
-      echo "Exiting. Bye!"
-      break
-      ;;
-    *)
-      echo "Invalid option. Try again."
-      ;;
-  esac
-done
+echo "Storage account: $STORAGE_ACCOUNT"  
+ echo "Container: $CONTAINER_NAME"
